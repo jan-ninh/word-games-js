ad4b6fa43ba49877261fffc778fc5adb6eb14ab4
Jan <janninh@gmx.de>
2025-11-04 15:13:46 +0100

    Rename files with numbered prefixes

---
+++ 01-rock-paper-scissors.js
@@ -0,0 +1,10 @@
+// TEST: console
+// console.log("Hallo");
+
+// TEST: Funktion
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log("2 + 3 =", add(2, 3));
+
+console.log(process.argv);
