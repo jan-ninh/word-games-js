37577e121190a8ec7dcf1dcd94044309f2e3ae8b
Jan <janninh@gmx.de>
2025-11-04 17:44:11 +0100

    basic gameplay

--- 01-rock-paper-scissors.js
+++ 01-rock-paper-scissors.js
@@ -1,10 +1,80 @@
-// TEST: console
-// console.log("Hallo");
+console.log("==============================================");
+console.log("============= Rock Paper Scissors ============");
+console.log("==============================================");
+console.log("");
 
-// TEST: Funktion
-// function add(a, b) {
-//   return a + b;
-// }
-// console.log("2 + 3 =", add(2, 3));
+const a = "rock";
+const b = "paper";
+const c = "scissors";
+const enemyChoices = [a, b, c];
 
-console.log(process.argv);
+const win = "=>Result:        You win!🎉";
+const lose = "=>Result:        You lose!";
+const draw = "=>Result:        Draw!";
+
+//======================================
+//======= 1)PlayerChoice ===============
+//======================================
+let playerChoice = process.argv[2];
+// playerChoice = a;
+console.log(`You chose:       ${playerChoice}`);
+
+//======================================
+//======= 2)EnemyChoice (random) =======
+//======================================
+const randomNum = Math.random() * 3;
+const flooredNum = Math.floor(randomNum);
+const enemyChoice = enemyChoices[flooredNum];
+console.log(`Computer chose:  ${enemyChoice}`);
+console.log("");
+
+// PLAYER CHOICE: ROCK
+if (playerChoice === "rock") {
+  switch (enemyChoice) {
+    case "rock":
+      console.log(draw);
+      break;
+    case "paper":
+      console.log(lose);
+      break;
+    case "scissors":
+      console.log(win);
+      break;
+    default:
+      console.log(`=>No Results!`);
+  }
+}
+
+// PLAYER CHOICE: PAPER
+if (playerChoice === "paper") {
+  switch (enemyChoice) {
+    case "rock":
+      console.log(win);
+    case "paper":
+      console.log(draw);
+      break;
+    case "scissors":
+      console.log(lose);
+      break;
+    default:
+      console.log(`=>No Results!`);
+  }
+}
+
+// PLAYER CHOICE: SCISSORS
+if (playerChoice === "scissors") {
+  switch (enemyChoice) {
+    case "rock":
+      console.log(lose);
+      break;
+    case "paper":
+      console.log(win);
+      break;
+    case "scissors":
+      console.log(draw);
+      break;
+    default:
+      console.log(`=>No Results!`);
+  }
+}
+console.log("\n");
